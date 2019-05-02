534b47d780edb78984c8d4bd4e0214c56e0573ca
Kedlawv <waldek.bus@gmail.com>
2019-05-02 18:08:41 +0200

    Path class lecture

---
+++ JavaAdvLinda/src/PathEx.java
@@ -0,0 +1,20 @@
+
+import java.io.IOException;
+import java.nio.file.*;
+
+public class PathEx {
+    public static void main(String[] args) throws IOException {
+
+        Path source = Paths.get("Files/test.txt");
+        System.out.println(source);
+
+        Path realPath = source.toRealPath();
+        System.out.println(realPath);
+
+        Path target = Paths.get("Files/new_test.txt");
+        Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+
+        
+
+    }
+}
