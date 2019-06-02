6b051ca99a8725bed7b4b13cf37d594ab0cb631e
Kedlawv <waldek.bus@gmail.com>
2019-06-02 19:12:39 +0200

    Java Tut Date-Time Excercises

--- PrivateExercises/src/learn/localDateTime/Main.java
+++ PrivateExercises/src/learn/localDateTime/Main.java
@@ -6,6 +6,7 @@ import java.time.format.DateTimeParseException;
 import java.time.format.TextStyle;
 import java.time.temporal.*;
 import java.util.Formatter;
+import java.util.List;
 import java.util.Locale;
 
 public class Main {
@@ -67,9 +68,9 @@ public class Main {
         // https://docs.oracle.com/javase/tutorial/datetime/iso/datetime.html
 
         DateTimeFormatter format = DateTimeFormatter.ofPattern("d MMM yyyy -- HH:mm z");
-        LocalDateTime ldt3 = LocalDateTime.of(2019,5,12,19,30);
+        LocalDateTime ldt3 = LocalDateTime.of(2019, 5, 12, 19, 30);
         ZoneId zid = ZoneId.of("Poland");
-        ZonedDateTime zdt = ZonedDateTime.of(ldt3,zid);
+        ZonedDateTime zdt = ZonedDateTime.of(ldt3, zid);
         String out1 = zdt.format(format);
 
         System.out.println("Not formatted: " + zdt);
@@ -78,15 +79,15 @@ public class Main {
 
         Instant timeStamp = Instant.now();
         System.out.println(timeStamp);
-        Instant timeStamp2 = Instant.now().plusSeconds(60*60+15);
+        Instant timeStamp2 = Instant.now().plusSeconds(60 * 60 + 15);
         System.out.println(timeStamp2);
         long secondsFromEpoch = Instant.ofEpochSecond(0l).until(Instant.now(), ChronoUnit.SECONDS);
         System.out.println(secondsFromEpoch);
 
-        timeStamp2 = timeStamp2.plus(2,ChronoUnit.HOURS);
+        timeStamp2 = timeStamp2.plus(2, ChronoUnit.HOURS);
         System.out.println(timeStamp2);
 
-        LocalDateTime ldtInst = LocalDateTime.ofInstant(timeStamp2,ZoneId.systemDefault());
+        LocalDateTime ldtInst = LocalDateTime.ofInstant(timeStamp2, ZoneId.systemDefault());
         System.out.println(ldt);
 
         System.out.println("\n\n\n\n______________Parse and Format____________");
@@ -97,12 +98,12 @@ public class Main {
 
 
         try {
-            LocalDate pfDate = LocalDate.parse(inputString,formatter);
+            LocalDate pfDate = LocalDate.parse(inputString, formatter);
             System.out.println(pfDate);
         } catch (DateTimeParseException e) {
             System.out.printf("%s in not parsable%n.", inputString);
             e.printStackTrace();
-        }catch (DateTimeException e){
+        } catch (DateTimeException e) {
             System.out.printf("%s is not a valid pattern?");
         }
 
@@ -122,7 +123,93 @@ public class Main {
         TemporalQuery<TemporalUnit> query = TemporalQueries.precision();
         System.out.printf("\nPrecision of LocalDate is %s%n", dateTA.query(query));
 
+        System.out.println("\n___________________Duration and Period____________________");
 
+        LocalDate dateNowDP = LocalDate.now();
+        LocalDate birthday = LocalDate.of(1979, 12, 31);
+
+        Period p = Period.between(birthday, dateNowDP);
+        long p2 = ChronoUnit.DAYS.between(birthday, dateNowDP);
+
+        System.out.printf("I am %s years old %s months and %s days, that is %s days total%n",
+                p.getYears(), p.getMonths(), p.getDays(), p2);
+
+        System.out.println("\n_____________Excercise 1______________");
+
+        reportLengthOfMonthsInGivenYear(2000);
+        reportLengthOfMonthsInGivenYearJava(2000);
+
+        System.out.println("\n________________Excercise 2 ______________");
+        getMondaysOfMonth(Month.JUNE);
+        System.out.println("_____________While loop imp_______________");
+        getMondaysOfMonthWhileLoop(Month.JUNE);
+        System.out.println("\n_____________Java tut imp_________________");
+        getMondaysOfMonthJava(Month.JUNE);
+
+
+    }
+
+    //1. Write an example that, for a given year, reports the length of each month within that year.
+
+    // My implementation
+    public static void reportLengthOfMonthsInGivenYear(int year) {
+        System.out.println("\n||||||||| My imp |||||||||||");
+        LocalDate date = LocalDate.of(year, 1, 1);
+        for (Month month : Month.values()) {
+            System.out.println(month + " is " + date.lengthOfMonth() + " days long.");
+            date = date.plusMonths(1);
+        }
+    }
+
+    // Java Tutorial implementation
+
+    public static void reportLengthOfMonthsInGivenYearJava(int year) {
+        System.out.println("\n||||||||||| Java Imp ||||||||||||");
+        for (Month month : Month.values()) {
+            YearMonth ym = YearMonth.of(year, month);
+            System.out.println(month + " is " + ym.lengthOfMonth() + " days long. ");
+        }
+    }
+
+    // 2. Write an example that, for a given month of the current year, lists all of the Mondays in that month.
+
+    public static void getMondaysOfMonth(Month month) {
+        LocalDate date = LocalDate.of(LocalDate.now().getYear(), month, 1);
+        for (int i = 1; i <= date.getMonth().maxLength(); i++) {
+            date = LocalDate.of(LocalDate.now().getYear(), month, i);
+            if (date.getDayOfWeek() == DayOfWeek.MONDAY) {
+                System.out.printf("%s is a %s%n", date, date.getDayOfWeek());
+            }
+        }
+
+    }
+
+    public static void getMondaysOfMonthWhileLoop(Month month) {
+        int year = LocalDate.now().getYear();
+        int day = 1;
+        LocalDate date = LocalDate.of(year, month, 1);
+        System.out.println(date);
+        LocalDate dateEnd = LocalDate.of(year,month,date.lengthOfMonth());
+        System.out.println(dateEnd);
+        while (ChronoUnit.DAYS.between(date,dateEnd) > -1) {
+            if (date.getDayOfWeek() == DayOfWeek.MONDAY) {
+                System.out.printf("%s is a %s%n", date, date.getDayOfWeek());
+            }
+            date = date.plusDays(1);
+        }
+
+    }
+    // Java tutorial implementation
+    public static void getMondaysOfMonthJava(Month month){
+        LocalDate date = Year.now().atMonth(month).atDay(1).
+                with(TemporalAdjusters.firstInMonth(DayOfWeek.MONDAY));
+        Month mth = date.getMonth();
+        while(mth == month){
+            System.out.printf("%s is a Monday%n",date);
+            date = date.with(TemporalAdjusters.next(DayOfWeek.MONDAY));
+            mth = date.getMonth();
+
+        }
 
     }
 
